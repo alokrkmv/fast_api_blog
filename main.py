cf416478db7e148569b4765b5fbe2c49bcd1f524
Alok <alokrkmv12@gmail.com>
2022-07-01 18:52:45 -0400

    Learnt how to do basic POST request in FASTAPI

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
 from typing import Optional
+
+from pydantic import BaseModel
+
+import uvicorn
 app = FastAPI()
 
 @app.get("/blog")
@@ -25,4 +29,18 @@ def fetch_id(id : int):
 @app.get("/blog/{id}/comments")
 def comments(id :int):
     return {"data":["1","2"]}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post("/blog")
+def create_blog(request:Blog):
+    print("Hey now brown cow")
+    return {"data":f"Blog is created with title as {request.title}"}
+
+# if __name__=='__main__':
+#     uvicorn.run("main:app", host="127.0.0.1",port=9000,reload=True)
+
 
